32d1081651fea25378bef97ddd9d1a429789d666
Justin <nebojustin@gmail.com>
2017-10-25 11:28:21 +0100

    create the server entry point

---
+++ bin/www.js
@@ -0,0 +1,10 @@
+// This is the application entry.
+import http from 'http';
+import app from '../app';
+
+// Set the app entry port
+app.set('port', process.env.PORT || 8000);
+
+const server = http.createServer(app);
+// server.listen(port);
+server.listen(app.get('port'));
