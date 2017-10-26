abd321350f2529640e04d3835d3327de0619db89
Justin <nebojustin@gmail.com>
2017-10-26 15:58:08 +0100

    add review in-memory data file

--- dummyDb/index.js
+++ dummyDb/index.js
@@ -1,6 +1,8 @@
 import recipes from './recipe';
+import reviews from './review';
 
 // This file exports all the im-memory data
 export default {
   recipes,
+  reviews
 };
---
+++ dummyDb/review.js
@@ -0,0 +1,8 @@
+// Review in-memory data
+const reviews = [{
+  id: 1,
+  recipeId: 1,
+  content: 'This recipe is truly tasty and nutritious',
+}];
+
+export default reviews;
