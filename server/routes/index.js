24ffcf1f6034756ccc68c17d5099099cc22c17d0
Justin <nebojustin@gmail.com>
2017-10-26 01:08:31 +0100

    add middleware function for validation on the add recipe API endpoint

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,7 +1,10 @@
-import controller from '../controllers';
+import controllers from '../controllers';
+import middlewares from '../middlewares';
 
-const recipeController = controller.recipe;
+const recipeController = controllers.recipe,
+  validate = middlewares.validation;
 
 export default (app) => {
-  app.post('/api/recipes', recipeController.addRecipe);
+  app.post('/api/recipes', validate.recipeRequiredInputs,
+    recipeController.addRecipe);
 };
