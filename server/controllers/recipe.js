c94d2f85f4d3c49afe511a068a3254f3f535b4d2
Justin <nebojustin@gmail.com>
2017-10-26 01:09:16 +0100

    refactor code for more clearity

--- server/controllers/recipe.js
+++ server/controllers/recipe.js
@@ -5,7 +5,7 @@ const recipes = db.recipes;
 export default {
   addRecipe(req, res) {
     recipes.push(req.body);
-    res.status(200).send({
+    return res.status(200).send({
       status: 'Success',
       message: 'Recipe added successfully',
     });
