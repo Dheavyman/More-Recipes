438fb006e051b015bf79e8e8b8b96ff579030ba3
Justin <nebojustin@gmail.com>
2017-10-28 13:00:11 +0100

    add upvote and downvote API endpoints

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,8 +1,10 @@
 import recipe from './recipe';
 import review from './review';
+import vote from './vote';
 
 // This file exports all the controllers
 export default {
   recipe,
-  review
+  review,
+  vote
 };
