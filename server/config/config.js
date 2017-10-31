f667a7727c67c82cc6c91c44e429ad8073bcf5b2
Justin <nebojustin@gmail.com>
2017-10-31 09:31:15 +0100

    supply database dialect explicitly

--- server/config/config.js
+++ server/config/config.js
@@ -6,14 +6,14 @@ const config = {
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DEV,
     host: process.env.DB_HOST,
-    dialect: process.env.DB_DIALECT,
+    dialect: 'postgres'
   },
   test: {
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_TEST,
     host: process.env.DB_HOST,
-    dialect: process.env.DB_DIALECT
+    dialect: 'postgres'
   },
   production: {
     use_env_variable: 'DATABASE_URL'
