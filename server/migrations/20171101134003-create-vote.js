2e1926bf6963b15bcc56fbde1913adff913198ae
Justin <nebojustin@gmail.com>
2017-11-01 16:10:23 +0100

    remove timestamps from vote migration file

--- server/migrations/20171101134003-create-vote.js
+++ server/migrations/20171101134003-create-vote.js
@@ -18,14 +18,6 @@ module.exports = {
       type: Sequelize.BOOLEAN,
       allowNull: false
     },
-    createdAt: {
-      allowNull: false,
-      type: Sequelize.DATE
-    },
-    updatedAt: {
-      allowNull: false,
-      type: Sequelize.DATE
-    }
   }),
   down: queryInterface => queryInterface.dropTable('Votes')
 };
