09b3e0c63cd8c5b413d53543b544d41514929caa
Justin <nebojustin@gmail.com>
2017-11-03 09:35:22 +0100

    add database name in config

--- server/config/config.js
+++ server/config/config.js
@@ -11,7 +11,7 @@ const config = {
   test: {
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_TEST,
+    database: 'more_recipes_test',
     host: process.env.DB_HOST,
     dialect: 'postgres'
   },
