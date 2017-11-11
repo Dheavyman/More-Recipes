b9d4ba16a1bb0773397f204b575444e0e4da02d3
Justin <nebojustin@gmail.com>
2017-11-11 16:44:51 +0100

    provide sensitive data as environment variables

--- server/helpers/sendEmail.js
+++ server/helpers/sendEmail.js
@@ -2,15 +2,17 @@ import nodemailer from 'nodemailer';
 
 const transport = nodemailer.createTransport({
   service: 'Gmail',
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.ADMIN_EMAIL,
-    pass: process.env.ADMIN_EMAIL_PASSWORD,
+    pass: process.env.ADMIN_PASSWORD,
   }
 });
 
 const sendEmail = (to, subject, message) => {
   const mailOptions = {
-    from: process.env.ADMIN_EMAIL,
+    from: `"No-reply" <${process.env.ADMIN_EMAIL}>`,
     to,
     subject,
     text: message,
