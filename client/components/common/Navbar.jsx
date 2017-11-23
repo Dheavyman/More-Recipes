3ce52c15b57c3eeb6006759f18dc698c515017bb
Justin <nebojustin@gmail.com>
2017-11-23 11:50:34 +0100

    initialize materialize components

--- client/components/common/Navbar.jsx
+++ client/components/common/Navbar.jsx
@@ -1,80 +1,118 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const Navbar = () => (
-  <div className="navbar-fixed">
-    <nav className="deep-orange darken-4">
-      <div className="nav-wrapper">
-        <Link
-          to="/"
-          id="logo"
-          className="brand-logo hide-on-small-only"
-        >
+import Signup from '../homepage/Signup';
+
+/**
+ * Class representing navbar
+ * 
+ * @class Navbar
+ * @extends {React.Component}
+ */
+class Navbar extends React.Component {
+  /**
+   * Component did mount method
+   *
+   * @returns{any} Initialize materialize components
+   * @memberof Navbar
+   */
+  componentDidMount() {
+    // Initialize materialize css side nav menu activator
+    $('.button-collapse').sideNav({
+      draggable: true,
+    });
+    // Initialize materialize dropdown class
+    $('.dropdown-button').dropdown({
+      hover: true,
+      belowOrigin: true,
+    });
+    // Initialize materialize modal
+    $('.modal').modal();
+  }
+  /**
+   * Render method
+   *
+   * @returns{object} React element
+   * @memberof Navbar
+   */
+  render() {
+    return (
+      <div className="navbar-fixed">
+        <nav className="deep-orange darken-4">
+          <div className="nav-wrapper">
+            <Link
+              to="/"
+              id="logo"
+              className="brand-logo hide-on-small-only"
+            >
           More-Recipes
-        </Link>
-        <a
-          data-activates="slide_out"
-          className="button-collapse"
-        >
-          <i className="material-icons">menu</i>
-        </a>
-        <ul className="right hide-on-med-and-down">
-          <li><Link to="/">Home</Link></li>
-          <li>
+            </Link>
             <a
-              className="dropdown-button dropdown-category"
-              data-activates="category"
+              data-activates="slide_out"
+              className="button-collapse"
             >
-              Category
+              <i className="material-icons">menu</i>
             </a>
-          </li>
-          <ul id="category" className="dropdown-content">
-            <li>
-              <a href="#!" className="collection-item black-text">Breakfast</a>
-            </li>
-            <li>
-              <a href="#!" className="collection-item black-text">Lunch</a>
-            </li>
-            <li>
-              <a href="#!" className="collection-item black-text">Dinner</a>
-            </li>
-            <li>
-              <a href="#!" className="collection-item black-text">Appetizer</a>
-            </li>
-            <li>
-              <a href="#!" className="collection-item black-text">Main</a>
-            </li>
-            <li>
-              <a href="#!" className="collection-item black-text">Dessert</a>
-            </li>
-          </ul>
-          <li>
-            <a
-              className="dropdown-button dropdown-user"
-              data-activates="user-control"
-            >
-              Welcome
-              <i className="material-icons large left">account_circle</i>
-            </a>
-            <ul id="user-control" className="dropdown-content">
+            <ul className="right hide-on-med-and-down">
+              <li><Link to="/">Home</Link></li>
               <li>
-                <a href="#signin" className="modal-trigger black-text">
-                  Sign in
-                  <i className="material-icons left">person</i>
+                <a
+                  className="dropdown-button dropdown-category"
+                  data-activates="category"
+                >
+              Category
                 </a>
               </li>
+              <ul id="category" className="dropdown-content">
+                <li>
+                  <a href="#!" className="collection-item black-text">Breakfast</a>
+                </li>
+                <li>
+                  <a href="#!" className="collection-item black-text">Lunch</a>
+                </li>
+                <li>
+                  <a href="#!" className="collection-item black-text">Dinner</a>
+                </li>
+                <li>
+                  <a href="#!" className="collection-item black-text">Appetizer</a>
+                </li>
+                <li>
+                  <a href="#!" className="collection-item black-text">Main</a>
+                </li>
+                <li>
+                  <a href="#!" className="collection-item black-text">Dessert</a>
+                </li>
+              </ul>
               <li>
-                <a href="#signup" className="modal-trigger black-text">
-                  Register
-                  <i className="material-icons left">folder</i>
+                <a
+                  className="dropdown-button dropdown-user"
+                  data-activates="user-control"
+                >
+              Welcome
+                  <i className="material-icons large left">account_circle</i>
                 </a>
+                <ul id="user-control" className="dropdown-content">
+                  <li>
+                    <a href="#signin" className="modal-trigger black-text">
+                  Sign in
+                      <i className="material-icons left">person</i>
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#signup" className="modal-trigger black-text">
+                  Register
+                      <i className="material-icons left">folder</i>
+                    </a>
+                  </li>
+                </ul>
               </li>
             </ul>
-          </li>
-        </ul>
+          </div>
+        </nav>
+        <Signup />
       </div>
-    </nav>
-  </div>
-);
+    );
+  }
+}
 
 export default Navbar;
