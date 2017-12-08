b130da23a4995383b09ae7c6c796f7e0df08f616
Justin Nebo <nebojustin@gmail.com>
2017-12-08 02:43:31 +0100

    enable access control allow headers for x-access-token used to pass my token

--- server/app.js
+++ server/app.js
@@ -20,7 +20,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept');
+    'Origin, X-Requested-With, Content-Type, Accept, X-Access-Token');
   next();
 });
 
